e2a3a70ef86cdf4ec038e1ca965190d54bec036c
mjdubose <matthew.dubose@gmail.com>
2021-11-06 12:07:55 -0500

    query params

--- index.js
+++ index.js
@@ -5,25 +5,45 @@ const {URL} = require('url');
 //The server should respond to all requets with a string
 
 // Start the server and have it listen on port 3000
+const objToString = (obj) => {
+    let string = '{';
+    for (const prop in obj) {
+        string = string + ' ' + prop + ": " + obj[prop] + ",";
+    }
 
-const server = http.createServer(function(req,res){
-    const baseURL =  req.protocol + '://' + req.headers.host + '/';
+    if (string.length === 1){
+        return '';
+    }
+        string = string.substring(0, string.length - 1);
+        string = string + ' }';
+
+    return string;
+}
+
+
+const server = http.createServer(function (req, res) {
+    const baseURL = req.protocol + '://' + req.headers.host + '/';
 
     //get the url and parse it.
-    const parsedUrl = new URL(req.url,baseURL);
+    const parsedUrl = new URL(req.url, baseURL);
     //get the path from the url;
     let path = parsedUrl.pathname;
-    const trimmedPath = path.replace(/^\/+|\/+$/g, '')
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
 
+    //get the query string as an object;
+    const queryStringObject = Object.fromEntries(parsedUrl.searchParams);
+
+    //get the http method
+    const method = req.method.toLowerCase();
 
     //send the response
     res.end('Hello World\n ');
 
     //log the request
-    console.log('request received on path: '+ trimmedPath);
+    console.log('request received on path: ' + trimmedPath + ' with method ' + method + ' with these query param parameters ' + objToString(queryStringObject));
 
 });
 
-server.listen(3000,function(){
+server.listen(3000, function () {
     console.log('The server is listening on port 3000 now');
 });
